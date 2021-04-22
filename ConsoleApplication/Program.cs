e3762354dce37496fcac20055637acc93bafd272
sarii96 <sara-96325@hotmail.com>
2021-04-22 09:36:25 -0600

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //optional 2
+            string name;
+            string location;
+
+            Console.Write("What is your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Where are you from? ");
+            location = Console.ReadLine();
+
+            //3.
+            Console.WriteLine($"Your name is {name}");
+            Console.WriteLine($"You are from {location}");
+
+            //4.
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"The current date is {currentDate.ToLongDateString()}");
+
+            //5.
+            var nextChristmans = new DateTime(2021, 12, 25);
+           
+            
+          //7.
+            Console.ReadKey();
+
+        }
+    }
+}
